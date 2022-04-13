25005c49c0fa2691773ee78682578c2b672ddd57
Marcus Wallace <mw23744@gmail.com>
2022-04-13 01:41:54 -0500

    Have a rough variant of cannon movement finished

--- src/TankGameFiles/Tank.java
+++ src/TankGameFiles/Tank.java
@@ -1,20 +1,36 @@
 package TankGameFiles;
 
 import edu.macalester.graphics.GraphicsObject;
+import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Rectangle;
+import edu.macalester.graphics.events.Key;
+import edu.macalester.graphics.events.KeyboardEvent;
 
 public class Tank {
+    private final double BARREL_LENGTH = 10;
     private double centerX;
     private double centerY;
+    Line cannon;
 
     public Tank(double centerX, double centerY, double width, double height) {
         this.centerX = centerX;
         this.centerY = centerY;
         GraphicsObject body = new Rectangle(centerX, centerY, );
-        GraphicsObject cannon = new Rectangle();
+        cannon = new Line(centerX, centerY, centerX+BARREL_LENGTH, centerY);
     }
 
-    public void setCannonAngle(double angle) {
-
+    public void setCannonAngle(KeyboardEvent key) {
+        int angle = 0;
+        if (key.getKey().equals(Key.valueOf("DOWN_ARROW"))){
+            angle -= 5;
+        }
+        if (key.getKey().equals(Key.valueOf("UP_ARROW"))){
+            angle += 5;
+        }
+        double x2 = cannon.getX1() + BARREL_LENGTH * Math.cos(Math.toRadians(angle));
+        double y2 = cannon.getY1() + BARREL_LENGTH * -Math.sin(Math.toRadians(angle));
+        cannon.setEndPosition(x2, y2);
     }
+
+    //Should fire cannon be here or in TankGame? (Marcus)
 }
--- src/TankGameFiles/TankGame.java
+++ src/TankGameFiles/TankGame.java
@@ -4,11 +4,17 @@ import edu.macalester.graphics.CanvasWindow;
 
 public class TankGame {
     private final int CANVAS_HEIGHT = 600;
-    private final int CANVAS_WIDTH = 1000;    
+    private final int CANVAS_WIDTH = 1000;
+    private CanvasWindow canvas;
 
     public TankGame() {
-        CanvasWindow canvas = new CanvasWindow("Tanks!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas = new CanvasWindow("Tanks!", CANVAS_WIDTH, CANVAS_HEIGHT);
     }
+
+    public void cannonControls() {
+        canvas.onKeyDown(event -> tank.setCannonAngle(event -> tank.setCannonAngle(event)));
+    }
+
     public static void main(String[] args) {
         new TankGame();
     }
