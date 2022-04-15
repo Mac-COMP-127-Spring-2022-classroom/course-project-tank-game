92d263f53cfc46254f3c6873ac9456a6ca8ea01d
bram-n <bnutt@macalester.edu>
2022-04-15 10:23:18 -0500

    added line for terrain

--- src/TankGameFiles/Terrain.java
+++ src/TankGameFiles/Terrain.java
@@ -4,6 +4,12 @@ import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.Line;
 
 public class Terrain extends GraphicsGroup{
-    // Line line =
+    private Line line
+    private static final MAX_X =1000;
+
+    public Terrain() {
+        Line line = new Line(0, 400, MAX_X, 400);
+        canvas.add(line)
+    }
     
 }
